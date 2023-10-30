1c8654d96754e1ec916f4ea4527f62bdf6ea5859
RaphaelIniesta <raphael_iniesta@yahoo.com>
2023-10-30 14:27:05 -0300

    Initial Commit

---
+++ NadaADeclarar/NadaADeclararApp.swift
@@ -0,0 +1,17 @@
+//
+//  NadaADeclararApp.swift
+//  NadaADeclarar
+//
+//  Created by Raphael Iniesta Reis on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NadaADeclararApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
