f9da81f68689c37acddbe439da7c638d9fd185f6
RaphaelIniesta <raphael_iniesta@yahoo.com>
2023-10-30 16:34:41 -0300

    Adicionar componente TextView

--- NadaADeclarar/Components/TextView.swift
+++ NadaADeclarar/Components/TextView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 enum TextType {
     case title
+    case subtitle
     case body
 }
 
@@ -21,14 +22,20 @@ struct TextView: View {
         self.type = type
     }
     var body: some View {
-        Text("\(text)")
-            .font(Font.custom("Montserrat", size: 48))
-            .fontWeight(.black)
+        Text(text)
+//            .font(Font.custom("Montserrat-Black", size: 48))
+            .font(type == .title ? Font.custom("Montserrat", size: 48) : Font.custom("Montserrat", size: 28))
+            .fontWeight(type == .title ? .black : .regular)
             .multilineTextAlignment(.center)
-            .foregroundColor(Color(red: 21 / 255, green: 0 / 255, blue: 34 / 255))
+            .foregroundColor(type == .title || type == .body ? Color(red: 54/255, green: 0/255, blue: 86/255) : Color(red: 45/255, green: 45/255, blue: 45/255))
     }
 }
 
 #Preview {
-    TextView("Banana", .title)
+    VStack{
+        TextView("Banana", .title)
+        TextView("Banana", .subtitle)
+        TextView("Banana", .body)
+    }
+
 }
