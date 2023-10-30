15cbece65b0336b9433962c05b4eb2a6e6a6b4ab
RaphaelIniesta <raphael_iniesta@yahoo.com>
2023-10-30 14:55:20 -0300

    Adicionar TextView

---
+++ NadaADeclarar/Components/TextView.swift
@@ -0,0 +1,18 @@
+//
+//  TextView.swift
+//  NadaADeclarar
+//
+//  Created by Raphael Iniesta Reis on 30/10/23.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextView()
+}
