cb841f4f043bc4b118945050da56e268851a98c5
RaphaelIniesta <raphael_iniesta@yahoo.com>
2023-10-31 17:00:51 -0300

    Adicionar Onboarding-three

---
+++ NadaADeclarar/Onboarding/Onboarding-three.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding-three.swift
+//  NadaADeclarar
+//
+//  Created by Raphael Iniesta Reis on 31/10/23.
+//
+
+import SwiftUI
+
+struct Onboarding_three: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding_three()
+}
