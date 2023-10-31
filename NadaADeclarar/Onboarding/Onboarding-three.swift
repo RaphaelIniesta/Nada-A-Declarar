16bc9d14315bd7dc310d72ec68301d737b1b3054
RaphaelIniesta <raphael_iniesta@yahoo.com>
2023-10-31 17:01:13 -0300

    Adicionar Onboarding-three

--- NadaADeclarar/Onboarding/Onboarding-three.swift
+++ NadaADeclarar/Onboarding/Onboarding-three.swift
@@ -2,17 +2,25 @@
 //  Onboarding-three.swift
 //  NadaADeclarar
 //
-//  Created by Raphael Iniesta Reis on 31/10/23.
+//  Created by Igor Bragança Toledo on 31/10/23.
 //
 
 import SwiftUI
 
-struct Onboarding_three: View {
+struct OnboardingThreeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .trailing){
+            VStack{
+                TextView("E não se esqueça dos pagamentos a terceiros, com a ajuda das anotações dos Alienígenas Fiscais!", .body)
+                    .padding(.vertical, 50)
+                ButtonRectangleView(placeholder: "Embarcar")
+            }
+            .frame(width: 407)
+            .padding(.leading, 660)
+        }
+        .background(Image("Onboarding 3") .ignoresSafeArea())
     }
 }
-
 #Preview {
-    Onboarding_three()
+    OnboardingThreeView()
 }
